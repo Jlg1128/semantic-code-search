841484cbdc7ae59c3b174f1b1300507ccd2cedbe
jianglonggui@corp.netease.com <jianglonggui@corp.netease.com>
2023-04-03 21:51:01 +0800

    fix:search env

--- src/search.ts
+++ src/search.ts
@@ -1,7 +1,7 @@
 import { DEFAULT_EMBEDDING_MODEL } from './const';
-import search from './searchFunction';
 import { loadEnv } from './util';
 loadEnv('example');
+import search from './searchFunction';
 
 search('用户名称', {model: DEFAULT_EMBEDDING_MODEL}).then(searchResult => {
   console.log('匹配的内容是', searchResult);
